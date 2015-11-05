2aa8b79b68e94092809b1371a1add98aedbba11b
JanMathis <paetzo@gmail.com>
2015-11-05 10:21:46 +0100

    letsdoit

---
+++ HelloWorld/src/HelloUser.java
@@ -0,0 +1,9 @@
+
+public class HelloUser {
+	public static String userName;
+	
+	public static void greetUser(){
+
+		System.out.println("Hello " + userName + "!") ;
+	}
+}
--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,18 +1,28 @@
-/**
- * 
- */
 
 /**
  * @author Tobias
  *
  */
+//use java.io to read String from console
+import java.io.*;
 public class HelloWorld {
 
 	/**
 	 * @param args
 	 */
+	
 	public static void main(String[] args) {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
 		System.out.println("Hello ASE2015 - how are you? :)");
+		System.out.print("Please enter your name: ");
+		try{
+			HelloUser.userName = in.readLine();
+		}
+		catch(Exception e) {	
+		}
+		
+		HelloUser.greetUser();
 	}
 
 }
